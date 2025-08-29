d029d2bb3a2c923a27c5a783b758459885e322d5
yjshin.cloud <yjshin.cloud@gmail.com>
2025-08-29 18:30:23 +0900

    Initial commit

---
+++ src/main/java/com/example/jpaadv/JpaadvApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaadv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaadvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaadvApplication.class, args);
+	}
+
+}
